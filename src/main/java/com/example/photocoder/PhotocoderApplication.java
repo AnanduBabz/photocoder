87976ff435e08e902bed422beeb7f496f5c49721
Admin <Admin@DESKTOP-6EOGL10>
2023-04-22 06:47:18 -0700

    initial commit

---
+++ src/main/java/com/example/photocoder/PhotocoderApplication.java
@@ -0,0 +1,13 @@
+package com.example.photocoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotocoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotocoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photocoder/PhotocoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photocoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotocoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
